e0de22aa0fa3f2ef81854816f9b797b8f7b7d6bd
Tolu Adesina <tadesina90@gmail.com>
2019-03-13 15:27:36 +0100

    transactions feature updated with reverse order

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,22 +1,24 @@
 $(document).ready(function(e){
-  var userId = localStorage.userId;
-  $.ajax({
-      method: 'GET',
-      url:  `http://localhost:3000/transactions?userId=${userId}`,
-      data: {get_param: 'value'}
-  }).done(data => {
-      $.each(data, (index,el) => {
-          console.log(el)
-          $('#transactions').append(`
-          <tr>
-          <td><i class="fa fa-circle st-${el.status}"></i></td>
-          <td>${el.transactionId}</td>
-          <td>${el.reference}</td>
-          <td>${el.transactionType}</td>
-          <td>${el.amount}</td>
-          <td>${el.timestamp}</td>
-          </tr>`
-        )
-      })
-  });
+    var userId = localStorage.userId;
+    $.ajax({
+        method: 'GET',
+        url:  `http://localhost:3000/transactions?userId=${userId}`,
+        data: {get_param: 'value'}
+    }).done(data => {
+        data1 = data;
+            data1 = data1.reverse();
+        $.each(data, (index,el) => {
+            // console.log(el); 
+            $('#transactions').append(`
+            <tr>
+            <td><i class="fa fa-circle st-${el.status}"></i></td>
+            <td>${el.transactionId}</td>
+            <td>${el.reference}</td>
+            <td>${el.transactionType}</td>
+            <td>${el.amount}</td>
+            <td>${el.timestamp}</td>
+            </tr>`
+          )
+        })
+    });
 });
